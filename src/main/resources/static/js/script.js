793c524db576e624adf8260c7856a357a3a6faa4
Sergey Royz <zjor.se@gmail.com>
2022-01-14 21:29:43 +0100

    wip

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,14 +1,47 @@
-const Counter = {
+const TrafficLight = {
     data() {
         return {
-            counter: 0
+            red: false,
+            yellow: false,
+            green: false,
         }
     },
     mounted() {
-        setInterval(() => {
-            this.counter++
-        }, 1000)
+        const COMMAND_RED = 'красный';
+        const COMMAND_YELLOW = 'жёлтый';
+        const COMMAND_GREEN = 'зелёный';
+
+        window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+        const recognition = new SpeechRecognition();
+        recognition.interimResults = true;
+        // recognition.lang = 'en-US';
+        recognition.lang = 'ru-RU';
+
+        recognition.addEventListener('result', async e => {
+            const transcript = Array.from(e.results)
+                .map(result => result[0])
+                .map(result => result.transcript)
+                .join('');
+            if (e.results[0].isFinal) {
+                console.log(transcript);
+                const command = transcript.toLocaleString().trim();
+                if (command == COMMAND_RED) {
+                    this.red = true; this.yellow = false; this.green = false;
+                } else if (command == COMMAND_YELLOW) {
+                    this.red = false; this.yellow = true; this.green = false;
+                } else if (command == COMMAND_GREEN) {
+                    this.red = false; this.yellow = false; this.green = true;
+                }
+                // await handleVoiceCommand(transcript.toLocaleString().trim(), client);
+            }
+        });
+
+        recognition.addEventListener('end', recognition.start);
+        recognition.start();
     }
 }
 
-Vue.createApp(Counter).mount('#counter')
+
+
+
+Vue.createApp(TrafficLight).mount('#traffic-light')
