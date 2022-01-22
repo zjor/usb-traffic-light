81bb625fe75ce7ccd1bc159844c288698180ed73
Sergey Royz <zjor.se@gmail.com>
2022-01-22 14:01:04 +0100

    wip

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-# Traffic Light API
+# Voice control for USB traffic light (Cleware USB-Ampel)
 
-- URL: `http://localhost:8080/swagger-ui/`
+- [Product](https://www.cleware-shop.de/USB-Ampel-EN)
 
-## Cleware USB-Ampel driver
+## API
 
-Code to control Cleware USB-Ampel with Java
+- UI: `http://localhost:8080/`
+- API URL: `http://localhost:8080/swagger-ui/`
+
+## Notes
 
 Make sure you have `javax.usb.properties` file on you classpath with the following content:
 ```
 javax.usb.services = org.usb4java.javax.Services
 ```
+
+## TODO
+
+- normal operation with FSM
+- connect 2 traffic light 
+
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,47 +1,83 @@
+class TrafficLightClient {
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    async getTrafficLightState() {
+        const res = await fetch(`${this.baseUrl}/api/traffic-light/state`);
+        return res.json();
+    }
+
+    async setTrafficLightState(red, yellow, green) {
+        const data = {red, yellow, green}
+        const res = await fetch(`${this.baseUrl}/api/traffic-light/state`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        return res.json()
+    }
+}
+
+const client = new TrafficLightClient('http://localhost:8080');
+
+function startSpeechRecognition(handler) {
+    window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.interimResults = true;
+    // recognition.lang = 'en-US';
+    recognition.lang = 'ru-RU';
+
+    recognition.addEventListener('result', async e => {
+        const transcript = Array.from(e.results)
+            .map(result => result[0])
+            .map(result => result.transcript)
+            .join('');
+        if (e.results[0].isFinal) {
+            const command = transcript.toLocaleString().trim();
+            await handler(command);
+        }
+    });
+    recognition.addEventListener('end', recognition.start);
+    recognition.start();
+}
+
+class FSM {
+    constructor(transitions) {
+    }
+}
+
 const TrafficLight = {
     data() {
         return {
             red: false,
             yellow: false,
             green: false,
+            lastCommand: '<none>'
         }
     },
     mounted() {
+
         const COMMAND_RED = 'красный';
         const COMMAND_YELLOW = 'жёлтый';
         const COMMAND_GREEN = 'зелёный';
+        const COMMAND_TRAFFIC_LIGHT = 'светофор';
 
-        window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-        const recognition = new SpeechRecognition();
-        recognition.interimResults = true;
-        // recognition.lang = 'en-US';
-        recognition.lang = 'ru-RU';
-
-        recognition.addEventListener('result', async e => {
-            const transcript = Array.from(e.results)
-                .map(result => result[0])
-                .map(result => result.transcript)
-                .join('');
-            if (e.results[0].isFinal) {
-                console.log(transcript);
-                const command = transcript.toLocaleString().trim();
-                if (command == COMMAND_RED) {
-                    this.red = true; this.yellow = false; this.green = false;
-                } else if (command == COMMAND_YELLOW) {
-                    this.red = false; this.yellow = true; this.green = false;
-                } else if (command == COMMAND_GREEN) {
-                    this.red = false; this.yellow = false; this.green = true;
-                }
-                // await handleVoiceCommand(transcript.toLocaleString().trim(), client);
+        startSpeechRecognition(async (command) => {
+            this.lastCommand = command;
+            if (command == COMMAND_RED) {
+                this.red = true; this.yellow = false; this.green = false;
+            } else if (command == COMMAND_YELLOW) {
+                this.red = false; this.yellow = true; this.green = false;
+            } else if (command == COMMAND_GREEN) {
+                this.red = false; this.yellow = false; this.green = true;
+            } else if (command == COMMAND_TRAFFIC_LIGHT) {
+                // TODO: run fsm
             }
-        });
-
-        recognition.addEventListener('end', recognition.start);
-        recognition.start();
+        })
     }
 }
 
-
-
-
-Vue.createApp(TrafficLight).mount('#traffic-light')
+Vue.createApp(TrafficLight).mount('#app')
